44faa4a1387940323271af4cc73f14cdb112a016
RUSHES02 <abhijaanganguly02@gmail.com>
2026-05-13 02:56:17 +0530

    view model

--- app/src/main/java/com/example/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/example/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -3,106 +3,100 @@ package com.example.cryptotracker.crypto.presentation.coin_list
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cryptotracker.core.domain.util.CoinListEvent
-import com.example.cryptotracker.core.domain.util.onError
-import com.example.cryptotracker.core.domain.util.onSuccess
-import com.example.cryptotracker.crypto.domain.CoinDataSource
-import com.example.cryptotracker.crypto.presentation.coin_details.DataPoint
+import com.example.cryptotracker.core.domain.util.NetworkError
+import com.example.cryptotracker.crypto.domain.CoinRepository
 import com.example.cryptotracker.crypto.presentation.modal.CoinUi
 import com.example.cryptotracker.crypto.presentation.modal.toCoinUi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 
 class CoinListViewModel(
-	private val coinDataSource: CoinDataSource
-): ViewModel() {
-	
-	private val _state = MutableStateFlow(CoinListState())
-	val state = _state
-		.onStart {
-			loadCoins()
-		}
-		.stateIn(
-			scope = viewModelScope,
-			started = SharingStarted.WhileSubscribed(5000L),
-			initialValue = CoinListState()
-		)
-	
-	private val _events = Channel<CoinListEvent>()
-	val events = _events.receiveAsFlow()
-	
-	fun onAction(action: CoinListAction){
-		when(action){
-			is CoinListAction.OnCoinClick -> {
-				selectedCoin(action.coinUi)
-			}
-			is CoinListAction.OnRefresh -> {
-				loadCoins()
-			}
-		}
-	}
-	
-	private fun selectedCoin(coinUi: CoinUi){
-		_state.update { it.copy(
-			selectedCoin = coinUi
-		) }
-		viewModelScope.launch {
-			coinDataSource
-				.getCoinDetails(
-					coinId = coinUi.id,
-					start = ZonedDateTime.now().minusDays(5),
-					end = ZonedDateTime.now()
-				)
-				.onSuccess { history ->
-					println(history)
-					val dataPoints = history
-						.sortedBy { it.dateTime }
-						.map {
-							DataPoint(
-								x = it.dateTime.hour.toFloat(),
-								y = it.priceUsd.toFloat(),
-								xLabel = DateTimeFormatter
-									.ofPattern("ha\nM/d")
-									.format(it.dateTime)
-							)
-						}
-					
-					_state.update {
-						it.copy(
-							selectedCoin = it.selectedCoin?.copy(
-								coinPriceHistory = dataPoints
-							)
-						)
-					}
-				}
-				.onError { error ->
-					_events.send(CoinListEvent.Error(error))
-				}
-		}
-	}
-	
-	private fun loadCoins(){
-		viewModelScope.launch {
-			_state.update { it.copy(loading = true) }
-			coinDataSource
-				.getCoins()
-				.onSuccess { coins ->
-					_state.update { it.copy(
-						loading = false,
-						coins = coins.map { it.toCoinUi() }
-						) }
-				}
-				.onError { error ->
-					_state.update { it.copy(loading = false) }
-					_events.send(CoinListEvent.Error(error))
-				}
-		}
-	}
+    private val repository: CoinRepository
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CoinListState())
+    val state = _state.asStateFlow()
+
+    private val _events = Channel<CoinListEvent>()
+    val events = _events.receiveAsFlow()
+
+    init {
+        observeCoins()
+        viewModelScope.launch {
+            refreshCoins()
+            repository.startRealtimeUpdates()
+        }
+    }
+
+    fun onAction(action: CoinListAction) {
+        when (action) {
+            is CoinListAction.OnCoinClick -> {
+                selectCoin(action.coinUi)
+            }
+
+            CoinListAction.OnRefresh -> {
+                viewModelScope.launch {
+                    refreshCoins()
+                }
+            }
+        }
+    }
+
+    private fun observeCoins() {
+        viewModelScope.launch {
+            repository.observeCoins()
+                .collect { coins ->
+                    _state.update {
+                        it.copy(
+                            loading = false,
+                            coins = coins.map {
+                                it.toCoinUi()
+                            }
+                        )
+                    }
+                }
+        }
+    }
+
+    private suspend fun refreshCoins() {
+        try {
+            _state.update {
+                it.copy(loading = true)
+            }
+            repository.refreshCoins()
+
+        } catch (e: Exception) {
+            _state.update {
+                it.copy(loading = false)
+            }
+
+            _events.send(
+                CoinListEvent.Error(
+                    NetworkError.UNKNOWN_ERROR
+                )
+            )
+        }
+    }
+
+    private fun selectCoin(
+        coinUi: CoinUi
+    ) {
+        _state.update {
+            it.copy(
+                selectedCoin = coinUi
+            )
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        viewModelScope.launch {
+            repository.stopRealtimeUpdates()
+        }
+    }
 }
